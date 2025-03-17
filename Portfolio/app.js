7eb812978ad197e7b3e997974fde4107dd033e2b
Chetan Tyagi <87710619+chetanty@users.noreply.github.com>
2025-03-17 18:15:04 +0530

    Update app.js

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,45 +1,44 @@
+document.addEventListener('keydown', function (e) {
+  if (e.ctrlKey && (e.key === 'u' || e.key === 'U')) {
+      e.preventDefault();
+  }
+});
 
-        document.addEventListener('keydown', function (e) {
-      if (e.ctrlKey && e.key === 'u' || e.key === 'U') {
-        e.preventDefault();
+let buttonColor = 0; // 0 for initial colors, 1 for swapped colors
+let fontReversed = false;
 
-      }
-    });
-    let buttonColor = 0; // 0 for initial colors, 1 for swapped colors
-    let fontReversed = false;
+function toggleButtonColor() {
+  const body = document.body;
+  const button = document.getElementById("mapMasterButton");
+  const darkModeButton = document.getElementById("darkModeToggle");
 
-    function toggleButtonColor() {
-        const body = document.body;
-        const button = document.getElementById("mapMasterButton");
+  if (buttonColor === 0) {
+      darkModeButton.innerHTML = "☀️"; 
+      body.style.backgroundColor = "#3e424b";
+      body.style.color = "#FFC30B";
 
-        if (buttonColor === 0) {
-            // Change to swapped colors and reverse the font
-            body.style.backgroundColor = "#3e424b";
-            body.style.color = "#FFC30B";
-            menu.style.backgroundColor= "#6d6e71";
-            button.style.backgroundColor = "#FFC30B";
-            button.style.color = "#000000";
-            fontReversed = true;
-            buttonColor = 1;
-        } else {
-            // Change back to initial colors and restore the font
-            body.style.backgroundColor = "#faf6ea";
-            body.style.color = "#000000";
-            menu.style.backgroundColor= "#dfdcd5";
-            button.style.backgroundColor = "#FFC30B";
-            button.style.color = "#000000";
-            fontReversed = false;
-            buttonColor = 0;
-        }
+      darkModeButton.style.backgroundColor = "#FFC30B"; 
 
-        // Toggle the font style
+      darkModeButton.style.color = "#000"; 
+      menu.style.backgroundColor = "#6d6e71"; 
+      menu.style.color = "#FFC30B"; 
+      buttonColor = 1; 
+  } else {
+    darkModeButton.innerHTML = "🌙"; 
+      body.style.backgroundColor = "#fefefe";
+      body.style.color = "#000000";
 
-    }
+      darkModeButton.style.backgroundColor = "#000"; 
+      darkModeButton.style.color = "#FFF"; 
+      menu.style.backgroundColor = "#dfdcd5"; 
+      menu.style.color = "#000000"; 
+      buttonColor = 0; 
+  }
+}
 
-    function openResume() {
-        // Replace 'resume.pdf' with the actual path to your resume
-        const resumeURL = 'assets/Chetan_Tyagi_Resume.pdf'; 
-    
-        // Open the resume in a new tab
-        window.open(resumeURL, '_blank');
-    }
+function openResume() {
+  const resumeURL = 'assets/Chetan_Tyagi_Resume.pdf'; 
+
+  // Open the resume in a new tab
+  window.open(resumeURL, '_blank');
+}
